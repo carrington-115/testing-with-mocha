577a223e7af8181a9a932bff125497ed72ba7ff8
Mark Carrington <frumarkcarrington@gmail.com>
2024-08-03 03:11:35 +0530

    learned how to write test for promises both for the pass and fail tests

--- README.md
+++ README.md
@@ -32,3 +32,8 @@ npx jest <path>
 2. _.toEqual()_: To equal is used to compare the values of objects or arrays.
 3. Testing for truthy and falsy values: We use `.toBeFalsy` to check if a value is `blank, null, undefined, zero, or false`, for example, finding the value of a variable in an object that does not exists. If a value is not any of these, then we can run a test that it is truthy.
 4. Error matchers: This is used the situation where we want to test a function or a block of code that they throw an error that we expect. The syntax for this is `.toThrow()`
+
+**Testing aynchronous code**
+
+- There are 3 types of asynchronous code that can be tested in jest: `callbacks, async-await, generators`
+-
--- tests/example.test.js
+++ tests/example.test.js
@@ -27,3 +27,45 @@ function division(a, b) {
 test("Testing division by zero error", () => {
   expect(() => division(1, 0)).toThrow();
 });
+
+function testingAsyncTest(callback) {
+  setTimeout(() => {
+    callback("hello world");
+  }, 1000);
+}
+
+// testing asynchronous functions
+
+// 1. callbacks
+
+test("Testing async callbacks", (done) => {
+  function callback(data) {
+    try {
+      expect(data).toBe("hello world");
+      done();
+    } catch (error) {
+      done(error);
+    }
+  }
+  testingAsyncTest(callback);
+});
+
+// 2. promise
+
+function testingPromiseAsync() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("hello world");
+    }, 1000);
+  });
+}
+
+// if promise passes
+test("Testing the promise", () => {
+  return expect(testingPromiseAsync()).resolves.toBe("hello world");
+});
+
+// if promise fails
+test("Testing if promise fails", () => {
+  return expect(testingPromiseAsync()).rejects.toThrow("error");
+});
